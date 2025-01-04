7058bc137d08452e5b9147886e4ae2219bb28ce4
Indranil102 <indranilsamanta2003@gmail.com>
2025-01-04 12:46:24 +0530

    some correction

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import Gallery from "../../component/Gallery/Gallery"
 import HeroSection from "../../component/Hoersection/HeroSection"
 import PopularProducts from "../../component/Popularproduct/PopularProducts"
 
-import Services from "../../component/Services/services"
+import Services from "../../component/Services/Services"
 import Testimonial from "../../component/Testimonial/Testimonial";
 
 
